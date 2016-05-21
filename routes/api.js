477ecedf5d60740b763e2753c4e412f3d853d62d
zhiqing <i@zhiqing.info>
2016-05-21 16:11:07 +0800

    add apis

--- routes/api.js
+++ routes/api.js
@@ -4,14 +4,46 @@ var User = require('../models/user');
 var Post = require('../models/post');
 
 router.get('/index', function(req, res, next) {
+  result = {
+    error: '',
+    result: null
+  }
   Post.get(null, function(err, posts) {
     if(err){
-      console.error(err);
-      req.flash('error', err);
-      return res.redirect('/');
+      result.error = err;
     }
-    res.end(JSON.stringify(posts));
+    result.result = posts;
+    res.json(result);
   });
 });
+
+router.get('/user/:username', function(req, res, next) {
+  var result = {
+    error: '',
+    result: null
+  }
+  var username = req.params.username;
+  User.get(username, function(err, user){
+    if(err){
+      result.error = err;
+      res.json(result);
+      return;
+    }else if(!user){
+      result.error = 'user not found!';
+      res.json(result);
+      return;
+    }
+    Post.get(user.name, function(err, posts) {
+      if(err){
+        result.error = err;
+      }
+      result.result = posts;
+      res.json(result);
+    });
+  });
+});
+
+
+
 
 module.exports = router;
